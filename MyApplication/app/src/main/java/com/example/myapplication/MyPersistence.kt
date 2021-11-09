0832c8e5fbbc14428dca197f05d02e01601376ab
Manfre-98 <62291576+Manfre-98@users.noreply.github.com>
2021-11-09 14:47:29 +0100

    Delete MyApplication directory

--- MyApplication/app/src/main/java/com/example/myapplication/MyPersistence.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-interface MyPersistence {
-    fun saveTrip(toWrite : String)
-    fun findTrips()  : MutableList<String>
-    fun removeLine(pos : Int)
-}
--- MyApplication/app/src/main/java/com/example/myapplication/MyUtility.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-import android.location.Location
-
-interface MyUtility {
-    fun ageMs(last : Location) : Long
-}
